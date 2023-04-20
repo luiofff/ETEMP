96d8bfd02729605c463c47d9862b12c8c89d92fd
luiofff <95875204+luiofff@users.noreply.github.com>
2023-04-20 12:04:13 +0300

    .

--- variants/main.js
+++ variants/main.js
@@ -2,7 +2,18 @@ const OpenBurger = () => {
     fJs.classList.toggle("first-stick")
     sJs.classList.toggle("second-stick")
     burgerMenuJs.classList.toggle("burger-open")
-    fullScreen.classList.toggle("")
 }
 
 burgerBtn.addEventListener('click', OpenBurger)
+
+
+// buttonts logic
+
+const SwipeBtns = () => {
+    var target = event.target;
+    
+}
+
+const buttonHeader = document.querySelector(".header-btn")
+
+buttonHeader.addEventListener("click", SwipeBtns)
