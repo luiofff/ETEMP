1c83696bd950f62ff374e0fc4d7130869327b9f7
luiofff <95875204+luiofff@users.noreply.github.com>
2023-04-22 19:02:58 +0300

    add new navbar

--- objectPage/main.js
+++ objectPage/main.js
@@ -1,7 +1,16 @@
 const isScrolling = () => {
-    const headerEl = document.querySelector('.primary-header')
+    const headerEl = document.querySelector('.for-backgroundcolor-logic')
+    const secondNAV = document.querySelector(".additional-block")
     let windowPosition = window.scrollY > 350
     headerEl.classList.toggle('active', windowPosition)
+
+    let windowPositionSecondNavOpen = window.scrollY > 20
+    secondNAV.classList.toggle('secondJs',  windowPositionSecondNavOpen)
+
+    if (window.scrollY > 450) {
+        secondNAV.classList.remove('secondJs')
+    }
+
 }
 window.addEventListener('scroll', isScrolling)
 
